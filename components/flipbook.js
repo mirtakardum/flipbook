70dd3553f0f61894fe903aa0e33abbd0855783bd
mirtakardum <mkardu02@fesb.hr>
2025-06-09 20:49:18 +0200

    added centering

--- components/flipbook.js
+++ components/flipbook.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React from "react";
+import React, { useState, useRef } from "react";
 import HTMLFlipBook from "react-pageflip";
 
 const images = [
@@ -15,16 +15,39 @@ const images = [
 ];
 
 function Flipbook(){
+    const [isCentered, setIsCentered] = useState(true);
+    const [pageClass, setPageClass] = useState("pr-100");
+    const bookRef = useRef();
+
+    const handleFlip = (e) => {
+    const currentPage = e.data;
+    const totalPages = bookRef.current.pageFlip().getPageCount();
+
+    if (currentPage === 0) {
+      setIsCentered(true);
+      setPageClass("pr-100");
+    } else if (currentPage === totalPages - 1) {
+      setIsCentered(true);
+      setPageClass("pl-100");
+    } else {
+      setIsCentered(false);
+      setPageClass("");
+    }
+    };
     return(
     <>
-    <div className="flex justify-center items-center h-screen bg-transparent overflow-hidden">
-        <div className="w-[90vw] h-[90vh] mt-50  max-w-[800px] max-h-[1000px]">
+    <div className={`flex items-center ${
+        isCentered ? "justify-center pt-20" : "justify-center pt-20"
+      } h-screen bg-transparent transition-all duration-500 overflow-hidden ${pageClass}`}>
+        <div className="w-[90vw] h-[90vh] max-w-[800px] max-h-[1000px]">
       <HTMLFlipBook
         width={500}
         height={500}
         showCover={true}
         drawShadow={false}
         size="stretch"
+        onFlip={handleFlip}
+        ref={bookRef}
       >
         {images.map((src, idx) => (
           <div key={idx} className="w-full h-full bg-transparent shadow-none">
